f992a5f23651db58d44732f6826f3f08e0dc0b7e
sleaze <70922563+tooslzy@users.noreply.github.com>
2026-02-05 11:23:02 -0500

    stuff

--- build.cmd
+++ build.cmd
@@ -29,5 +29,5 @@ if not exist %USERPROFILE%\vcpkg\vcpkg.exe (
 cmake -B build -S . -DCMAKE_TOOLCHAIN_FILE=%USERPROFILE%\vcpkg\scripts\buildsystems\vcpkg.cmake -A x64 -DCMAKE_BUILD_TYPE=Release > .\build-1.log 2>&1
 cmake --build build --config Release -- /m > .\build-2.log 2>&1
 
-pause >nul 2>nul
+@REM pause >nul 2>nul
 exit /b 0
